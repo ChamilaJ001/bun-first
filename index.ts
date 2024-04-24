410dfbcc2b38ffcd613118346278e7d511192217
Chamila Jayasinghe <official.chamilaj@gmail.com>
2024-04-24 11:42:17 +0530

    add routes

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response("Hello World!");
+    const url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      return new Response("Hello World!");
+    }
+
+    if (url.pathname === "/about") {
+      return new Response("About us!");
+    }
+
+    return new Response("404 | Not found");
   },
 });
 
